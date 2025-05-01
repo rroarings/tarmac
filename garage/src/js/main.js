9f9866722514e76d569c2ba1b0d73c14e19f3c40
ovlack <nick.calvo007@gmail.com>
2025-05-01 08:05:53 -0500

    feat: add lenis smooth scroll

--- garage/src/js/main.js
+++ garage/src/js/main.js
@@ -1,2 +1,11 @@
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+import Lenis from 'lenis'
+
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+lenis.on('scroll', (e) => {
+});
