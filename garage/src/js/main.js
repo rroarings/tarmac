130b8a6ed0e7579d3c0619fc5d6ce40c64d24420
ovlack <steelgseries@hotmail.com>
2025-05-22 15:52:47 -0500

    Revert "Merge pull request #36 from rroarings/feat/astro-contact-form"
    This reverts commits adding astro into the main branch reversing
    changes

--- garage/src/js/main.js
+++ garage/src/js/main.js
@@ -1,10 +1,11 @@
 // Import all of Bootstrap's JS
-import * as bootstrap from "bootstrap";
-import Lenis from "lenis";
+import * as bootstrap from 'bootstrap'
+import Lenis from 'lenis'
 
 // Initialize Lenis
 const lenis = new Lenis({
   autoRaf: true,
 });
 
-lenis.on("scroll", (e) => {});
+lenis.on('scroll', (e) => {
+});
