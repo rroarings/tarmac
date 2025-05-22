8b0be5076352b7c59af548fddd2708758acee59d
ovlack <steelgseries@hotmail.com>
2025-05-22 14:13:09 -0500

    styles: file formatting/lint

--- garage/src/js/breakpoints.js
+++ garage/src/js/breakpoints.js
@@ -2,24 +2,24 @@
 
 function updateBreakpoint() {
   const width = window.innerWidth;
-  const breakpoint = document.getElementById('breakpoint');
+  const breakpoint = document.getElementById("breakpoint");
 
   // Check window width and update breakpoint text based on Bootstrap's breakpoints
   if (width < 576) {
-    breakpoint.textContent = 'XS';
+    breakpoint.textContent = "XS";
   } else if (width >= 576 && width < 768) {
-    breakpoint.textContent = 'SM';
+    breakpoint.textContent = "SM";
   } else if (width >= 768 && width < 992) {
-    breakpoint.textContent = 'MD';
+    breakpoint.textContent = "MD";
   } else if (width >= 992 && width < 1200) {
-    breakpoint.textContent = 'LG';
+    breakpoint.textContent = "LG";
   } else if (width >= 1200 && width < 1400) {
-    breakpoint.textContent = 'XL';
+    breakpoint.textContent = "XL";
   } else {
-    breakpoint.textContent = 'XXL';
+    breakpoint.textContent = "XXL";
   }
 }
 
 // Initialize the breakpoint size on page load and resize
-window.addEventListener('load', updateBreakpoint);
-window.addEventListener('resize', updateBreakpoint);
+window.addEventListener("load", updateBreakpoint);
+window.addEventListener("resize", updateBreakpoint);
--- garage/src/js/main.js
+++ garage/src/js/main.js
@@ -1,11 +1,10 @@
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
-import Lenis from 'lenis'
+import * as bootstrap from "bootstrap";
+import Lenis from "lenis";
 
 // Initialize Lenis
 const lenis = new Lenis({
   autoRaf: true,
 });
 
-lenis.on('scroll', (e) => {
-});
+lenis.on("scroll", (e) => {});
