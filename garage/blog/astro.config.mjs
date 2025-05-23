68cc7216b16e15dc18d78dbe2c0dae9ef692298d
ovlack <steelgseries@hotmail.com>
2025-05-22 20:07:35 -0500

    feat: update Astro configuration to include Tailwind CSS plugin

--- garage/blog/astro.config.mjs
+++ garage/blog/astro.config.mjs
@@ -7,6 +7,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
