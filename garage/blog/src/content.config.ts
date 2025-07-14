8cc35e71614bdec563b061fc3500a926fb428aa2
ovlack <steelgseries@hotmail.com>
2025-07-14 11:30:52 -0500

    refactor(blog): update schema definition to use image() for heroImage

--- garage/blog/src/content.config.ts
+++ garage/blog/src/content.config.ts
@@ -5,15 +5,16 @@ const blog = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
   loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
   // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-    relatedDrivers: z.array(reference("drivers")).optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image(),
+      heroImageAlt: z.string().optional(),
+      relatedDrivers: z.array(reference("drivers")).optional(),
+    }),
 });
 
 // F1 Drivers Collection
